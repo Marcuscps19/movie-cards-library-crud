9052b9c04fa7b8f3b5f5959f77f02719a551963c
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-30 12:32:39 -0300

    feat: adiciona configuraçoes para gh-pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         <Route path="/movies/new" component={ NewMovie } />
